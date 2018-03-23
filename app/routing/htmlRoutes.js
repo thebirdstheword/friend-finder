0afaac46b0d82318a2048309588a5f4119a5b9ec
Emily Bird <embird@emilys-mbp.attlocal.net>
2018-03-23 18:08:28 -0500

    fixed modal route and added html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,12 +1,13 @@
+var express = require("express");
 var path = require("path");
 
 // Basic route that sends the user first to the AJAX Page
 module.exports = function(app) {
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "/../public/home.html"));
-    });
-    
-    app.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname, "/../public/survey.html"));
-    });
-}
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  });
+};
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 // Sets up the Express App
 // =============================================================
@@ -9,9 +10,12 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+// Requires and sets the routes in this file
 require("./app/routing/htmlRoutes")(app);
 require("./app/routing/apiRoutes")(app);
 
